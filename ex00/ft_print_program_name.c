72ffca87abd669a0bdbd6418be499e98c8393c85
ikram <114251496+ikramkey@users.noreply.github.com>
2025-08-31 13:20:01 +0100

    done

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -1,16 +1,21 @@
 #include <unistd.h>
 
-int main(int argc , char **argv){
-    if (argc == 1)
-    {
-        char *fileName = argv[0];
-        int i = 0;
-        while( fileName[i] != '\0'){
-            write (1, &fileName[i], 1);
-            i++;
-        }
-    }
-    
-   write (1, "\n",1);
-    
+
+void ft_putstr(char *str){
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	
+}
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		ft_putstr(argv[0]);
+	}
+
+	write(1, "\n", 1);
 }
--- ex01/ft_print_params.c
+++ ex01/ft_print_params.c
@@ -1,8 +1,28 @@
  #include <unistd.h>
 
+void ft_putstr(char *str){
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	
+}
 
 int main(int argc , char **argv){
-
-    
+    if (argc > 1)
+    {
+        int i = 1;// did not ask to print name of program so skip oy
+        while (i < argc)
+        {
+            ft_putstr(argv[i]);
+            write(1, "\n", 1);
+            i++;
+        }
+        
+    }else{
+         write(1, "\n", 1);
+    }
 
 }
